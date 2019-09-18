54447953c63dc958f5197fbd7ca407dde4a815e4
Sharoon Thomas <sharoon.thomas@fulfil.io>
2019-09-18 06:09:47 -0500

    Make batch creation more efficient

--- kitchensink/shipment_batcher.py
+++ kitchensink/shipment_batcher.py
@@ -39,16 +39,12 @@ def create_optimal_batches(shipments=None, dry=False):
     if not shipments:
         shipments = _get_shipments(warehouse_id)
 
-    locations = _get_locations(warehouse_id)
-    print("Product locations: {}".format(len(locations)))
-
     singles = [s for s in shipments if len(s['inventory_moves']) == 1]
     print("Single line shipments: {}".format(len(singles)))
 
-    _identify_singles_batch(singles, locations)
+    _identify_singles_batch(singles)
     _identify_multi_batch(
         [s for s in shipments if len(s['inventory_moves']) > 1],
-        locations
     )
 
     _print_batches(shipments)
@@ -88,20 +84,6 @@ def _print_batches(shipments):
     print(AsciiTable(data).table)
 
 
-def _get_locations(warehouse_id):
-    """
-    Fetch all locations
-    """
-    ProductLocation = fulfil.model('product.warehouse.location')
-    locations = ProductLocation.search_read_all([
-        ('warehouse', '=', warehouse_id)
-    ], [('sequence', 'asc')], ['name', 'product'])
-    return {
-        l['product']: l['name']
-        for l in locations
-    }
-
-
 def _get_shipments(warehouse_id):
     """
     Return shipments that qualify to be batched
@@ -115,19 +97,25 @@ def _get_shipments(warehouse_id):
         ('shipping_batch', '=', None),
         # ('planned_date', '=', '2019-09-12'),
         ('carrier', '!=', None),
-    ], None, None)
-    return Shipment.serialize(shipment_ids)
+    ], None, 5000)
+    shipment_data = []
+    print('Collecting shipments')
+    per_chunk = 250
+    for index, chunk in enumerate(chunked(shipment_ids, per_chunk)):
+        print('{} --> {}'.format(
+            (index * per_chunk),
+            ((index - 1) * per_chunk)
+        ))
+        shipment_data.extend(Shipment.serialize(chunk))
+    return shipment_data
 
 
-def _identify_multi_batch(shipments, locations):
+def _identify_multi_batch(shipments, batch_size=BATCH_SIZE):
     """
     Given a collection of multi line shipments
     create batches of Batch size.
     """
-    key = lambda shipment: sorted([
-        locations.get(move['product']['id'], 'Z')
-        for move in shipment['outgoing_moves']
-    ])[0]
+    key = lambda shipment: shipment['inventory_moves'][0]['warehouse_location_name'] or 'Z'
     for index, chunk in \
             enumerate(chunked(sorted(shipments, key=key), BATCH_SIZE), 1):
         _set_batch(chunk, "Multi-Item Batch {:0>3}".format(
@@ -135,20 +123,21 @@ def _identify_multi_batch(shipments, locations):
         ))
 
 
-def _identify_singles_batch(shipments, locations):
+def _identify_singles_batch(shipments, batch_size=BATCH_SIZE):
     """
     Create singles batches with specific items
     """
-    key = lambda s: s['outgoing_moves'][0]['product']['id']     # noqa
+    key = lambda s: s['inventory_moves'][0]['product']['id']     # noqa
     unbatched = defaultdict(list)
     for product, p_shipments in groupby(sorted(shipments, key=key), key=key):
         p_shipments = list(p_shipments)
-        if len(p_shipments) > BATCH_SIZE:
+        move = p_shipments[0]['inventory_moves'][0]
+        if len(p_shipments) > batch_size:
             _set_batch(p_shipments, "Single line item: {}".format(
-                p_shipments[0]['outgoing_moves'][0]['product']['code']
+               move['product']['code']
             ))
         else:
-            unbatched[locations.get(product, 'Z-NO-LOC')].extend(
+            unbatched[move['warehouse_location_name'] or 'Z-NO-LOC'].extend(
                 p_shipments
             )
 
@@ -156,8 +145,9 @@ def _identify_singles_batch(shipments, locations):
     ordered_unbatched = []
     for location in sorted(unbatched.keys()):
         ordered_unbatched.extend(unbatched[location])
-    # Now create a batch for them in buckets of 16
-    for index, chunk in enumerate(chunked(ordered_unbatched, BATCH_SIZE), 1):
+
+    # Now create a batch for them in buckets of batch size
+    for index, chunk in enumerate(chunked(ordered_unbatched, batch_size), 1):
         _set_batch(chunk, "Assorted Single Item Batch {:0>3}".format(index))
 
 
